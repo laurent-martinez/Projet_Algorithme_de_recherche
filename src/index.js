9268766490497868aee71d5faa0238633aebe35a
laurent-martinez <martilaurier@gmail.com>
2022-07-20 14:16:33 +0200

    dataset not recognize on svg, change for icon

--- src/index.js
+++ src/index.js
@@ -74,9 +74,7 @@ const manageTags = () => {
         };
         tagList.push(tag); // je mets dans mon tableau tous ce que je click
         tags.innerHTML += `
-                    <button class="tag tag-${tag.type}">${tag.value} <svg width="20" class="close-tag" data-value="${tag.value}" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12.59 6L10 8.59L7.41 6L6 7.41L8.59 10L6 12.59L7.41 14L10 11.41L12.59 14L14 12.59L11.41 10L14 7.41L12.59 6ZM10 0C4.47 0 0 4.47 0 10C0 15.53 4.47 20 10 20C15.53 20 20 15.53 20 10C20 4.47 15.53 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18Z" fill="white"/>
-                    </svg>
+                    <button class="tag tag-${tag.type}">${tag.value}<i class="fa-solid fa-xmark close-tag" data-value="${tag.value}"></i> 
                     </button>
                 `;
         const tagResult = filter.recipesByTags(tag); // je trie avec ma class Filter
